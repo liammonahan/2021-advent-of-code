5b28ea0eb02fe882510603779a24fd3fc5f03478
Liam Monahan <liam.monahan@captivateiq.com>
2021-12-12 02:35:54 -0500

    day 12 refactor

--- day12/day12.py
+++ day12/day12.py
@@ -14,56 +14,36 @@ for line in lines:
     G[b].add(a)  # add both directions
 
 
+def lower2(_visited):
+    has2 = 0
+    for k, v in _visited.items():
+        if k.islower() and v > 1:
+            has2 += 1
+    return has2
+
+
+def explore(current, dest, count, visited, double_visit):
+    visited[current] += 1
+    if current == dest:
+        count += 1
+    else:
+        for neighbor in G[current]:
+            if neighbor.isupper() or visited[neighbor] == 0:
+                count = explore(neighbor, dest, count, visited, double_visit)
+            elif double_visit and neighbor not in ['start', 'end'] and lower2(visited) < 1:
+                count = explore(neighbor, dest, count, visited, double_visit)
+    visited[current] -= 1
+    return count
+
+
 def part1():
-    visited = set()
-
-    def explore(current, dest, count):
-        visited.add(current)
-
-        if current == dest:
-            count += 1
-        else:
-            for neighbor in G[current]:
-                if neighbor.isupper() or neighbor not in visited:
-                    count = explore(neighbor, dest, count)
-
-        if current in visited:
-            visited.remove(current)
-
-        return count
-
-    return explore(current='start', dest='end', count=0)
+    visited = defaultdict(int)
+    return explore(current='start', dest='end', count=0, visited=visited, double_visit=False)
 
 
 def part2():
     visited = defaultdict(int)
-
-    def lower2(_visited):
-        has2 = 0
-        for k, v in _visited.items():
-            if k.islower() and v > 1:
-                has2 += 1
-        return has2
-
-    def explore(current, dest, count):
-        visited[current] += 1
-
-        if current == dest:
-            count += 1
-        else:
-            for neighbor in G[current]:
-                if neighbor.isupper():
-                    count = explore(neighbor, dest, count)
-                elif visited[neighbor] == 0:
-                    count = explore(neighbor, dest, count)
-                elif neighbor not in ['start', 'end'] and lower2(visited) < 1:
-                    count = explore(neighbor, dest, count)
-
-        visited[current] -= 1
-
-        return count
-
-    return explore(current='start', dest='end', count=0)
+    return explore(current='start', dest='end', count=0, visited=visited, double_visit=True)
 
 
 print(part1())
